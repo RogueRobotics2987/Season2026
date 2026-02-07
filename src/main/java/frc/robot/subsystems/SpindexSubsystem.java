cef6271f8a0dd35e448e8903b7a05f7ed1667386
BrandonS2987 <brandonsandmeier@icloud.com>
2026-02-07 14:58:57 -0600

    added partial velocity pid

--- src/main/java/frc/robot/subsystems/SpindexSubsystem.java
+++ src/main/java/frc/robot/subsystems/SpindexSubsystem.java
@@ -7,16 +7,18 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 
 public class SpindexSubsystem extends SubsystemBase {
 
-  private final TalonFX SpindexMotor = new TalonFX(10, "rio"); //change rio?
+  private final TalonFX SpindexMotor = new TalonFX(20, "rio"); //change rio?
   /** Creates a new spindexSubsystem. */
   public SpindexSubsystem() {
     var slot0Configs = new Slot0Configs();
-    slot0Configs.kP = 0.1; // An error of 1 rotation results in 2.4 V output
+    slot0Configs.kP = 20; // An error of 1 rotation results in 2.4 V output
     slot0Configs.kI = 0; // no output for integrated error
-    slot0Configs.kD = 0.1; // A velocity of 1 rps results in 0.1 V output
+    slot0Configs.kD = 0; // A velocity of 1 rps results in 0.1 V output
 
     SpindexMotor.getConfigurator().apply(slot0Configs);
     System.out.println("yo");
@@ -26,5 +28,18 @@ public class SpindexSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    final VelocityVoltage m_request = new VelocityVoltage(100).withSlot(0); //leave pos blank
+    SpindexMotor.setControl(m_request);
+  }
+
+  public void start(){
+   final VelocityVoltage m_request = new VelocityVoltage(placeholder).withSlot(0);
+  }
+  public void stop(){
+   final VelocityVoltage m_request = new VelocityVoltage(0).withSlot(0);
   }
 }
+
+  
+// add two public void functions, start & stop
+// in these functions, it should set the velocity to a certain speed, defined in constants
