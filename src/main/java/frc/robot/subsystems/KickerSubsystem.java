d98bb16853150ec09aaa83548bf5937ee655a94f
danort01 <dannalynorth@gmail.com>
2026-02-10 20:06:26 -0600

    set mechanism gear ratio in constatants

--- src/main/java/frc/robot/subsystems/KickerSubsystem.java
+++ src/main/java/frc/robot/subsystems/KickerSubsystem.java
@@ -6,28 +6,46 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.controls.VelocityVoltage;
+import frc.robot.Constants;
 
 public class KickerSubsystem extends SubsystemBase {
-  private final TalonFX motorkicker11 = new TalonFX(11, "rio"); //change rio?
+  // <type> <name> = <value>;
+
+
+  // <type> <name> = <value>;
+  public final TalonFX motorkicker11 = new TalonFX(11, "rio"); //change rio?
   /** Creates a new KickerSubSystem. */
   public KickerSubsystem() { 
 
 
     System.out.println ("uyuioiytriytr");
-    var slot0Configs = new Slot0Configs();
+    // Setting Up PID Controller;
+    var slot0Configs = new Slot0Configs(); // Creating a new object (instantiation)
     slot0Configs.kP = 20; // An error of 1 rotation results in 2.4 V output
     slot0Configs.kI = 0; // no output for integrated error
     slot0Configs.kD = 0; // A velocity of 1 rps results in 0.1 V output
 
+    // ============ 
+    
+    // equivalent 
     motorkicker11.getConfigurator().apply(slot0Configs);
+    // ============
 
+   
     var feedback = new FeedbackConfigs();
-    feedback.SensorToMechanismRatio =  1;
+    feedback.SensorToMechanismRatio =  Constants.Kicker16;
+
+    // ============
+    // split into 2 lines of code here
+   
+    // equivalent
     motorkicker11.getConfigurator().apply(feedback);
+    // ===========
   }
 
   // Add two public void functions START and STOP sets velocity START sets veloctity to a 
