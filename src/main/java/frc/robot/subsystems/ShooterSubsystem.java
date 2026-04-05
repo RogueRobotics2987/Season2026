d03b86cc3bbd4f50a0986fd3588c899bc0dc0ccc
BrandonS2987 <brandonsandmeier@icloud.com>
2026-04-05 17:21:41 -0500

    Cleaned up code (kept beautiful comments)

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -131,10 +131,10 @@ public class ShooterSubsystem extends SubsystemBase  {
   }
 
   public void StartREV() { // JEFF DOESNT LIKE THE NAME
-    double KickerSpeed = SmartDashboard.getNumber("Kicker Speed", Constants.kickerOnSpeed); //15?
+    double KickerSpeed = SmartDashboard.getNumber("Kicker Speed", Constants.kickerOnSpeed);
     double ShooterSpeed = SmartDashboard.getNumber("Shooter Speed", Constants.shooterOnSpeed);
 
-    m_kickerRequest = new VelocityVoltage(KickerSpeed).withSlot(0); // COREY SAID COULD BE MEMBER VARIBLES
+    m_kickerRequest = new VelocityVoltage(KickerSpeed).withSlot(0);
     m_shooterRequest = new VelocityVoltage(ShooterSpeed).withSlot(0);
     motorKicker.setControl(m_kickerRequest.withVelocity(KickerSpeed));
     motorShooterWheels.setControl(m_shooterRequest.withVelocity(ShooterSpeed));
@@ -148,19 +148,7 @@ public class ShooterSubsystem extends SubsystemBase  {
   }
 
   public double CalculateShooterElevation(double Distance) {
-    // return 346132 + -19997 * Distance + 504 * Math.pow(Distance, 2) + -7.26 *Math.pow(Distance, 3) + 0.0652 * Math.pow(Distance, 4) + -3.74E-04 * Math.pow(Distance, 5) + 1.34E-06 * Math.pow(Distance, 6) + -2.73E-09 * Math.pow(Distance, 7) + 2.43E-12 * Math.pow(Distance, 8);   //calculated on 2/17
-    // return 346132.119250917 + -19996.8582755326 * Distance + 504.49244271105 * Math.pow(Distance, 2) + -7.25940609155529 * Math.pow(Distance, 3) + 0.0651653318101579 * Math.pow(Distance, 4) + -0.000373679751502965 * Math.pow(Distance, 5) + 0.00000133674724588682 * Math.pow(Distance, 6) + -0.00000000272739454061091 * Math.pow(Distance, 7) + 2.43003670597437E-12 * Math.pow(Distance, 8);
-    // return 55.3044635576478000000 + -6.36094409009439000000 * Distance + 0.32149925333374000000000000 * Math.pow(Distance, 2) + -0.00940821025540215 * Math.pow(Distance, 3) + 0.00017664574156125100000000 * Math.pow(Distance, 4) + -0.00000222537607870770000000 * Math.pow(Distance, 5) + 0.00000001906849698739800000 * Math.pow(Distance, 6) + -0.000000000109846770121158000 * Math.pow(Distance, 7) + 0.000000000000407556427043987000 * Math.pow(Distance, 8) + -0.000000000000000880327958574075 * Math.pow(Distance, 9) + 0.000000000000000000841471744895 * Math.pow(Distance, 10);
-    // return -17658.1810607820000000000 + 1699.70405971566000000000 * Distance + -73.37785024238110000000000000 * Math.pow(Distance, 2) + 1.87084632575126000000000000 * Math.pow(Distance, 3) + -0.03119461289815020000000000 * Math.pow(Distance, 4) + 0.00035541645787583600000000 * Math.pow(Distance, 5) + -0.00000280208223621810000000 * Math.pow(Distance, 6) + 0.000000015093516911952800000000 * Math.pow(Distance, 7) + -0.00000000005315791777 * Math.pow(Distance, 8) + 0.000000000000110529019078551000 * Math.pow(Distance, 9) + -0.000000000000000103026272855651 * Math.pow(Distance, 10);
-    // return -166.4705896121040000000 + 13.04197682346640000000 * Distance + -0.45355909682121900000000000 * Math.pow(Distance, 2) + 0.00921255553273902000000000 * Math.pow(Distance, 3) + -0.00012094532664654600000000 * Math.pow(Distance, 4) + 0.00000107180402830985000000 * Math.pow(Distance, 5) + -0.00000000649120754999035000 * Math.pow(Distance, 6) + 0.000000000026527023622102900000 * Math.pow(Distance, 7) + -0.000000000000070012638661799200 * Math.pow(Distance, 8) + 0.000000000000000107793069638857 * Math.pow(Distance, 9) + -0.000000000000000000073545521976 * Math.pow(Distance, 10);
     return (-244.2500163983830000000 + 18.33245249610840000000 * Distance + -0.61129148599016500000000000 * Math.pow(Distance, 2) + 0.01191623128273760000000000 * Math.pow(Distance, 3) + -0.00015029375962969700000000 * Math.pow(Distance, 4) + 0.00000128096529744011000000 * Math.pow(Distance, 5) + -0.00000000746980202580367000 * Math.pow(Distance, 6) + 0.000000000029425639296377200000 * Math.pow(Distance, 7) + -0.000000000000074946047543541000 * Math.pow(Distance, 8) + 0.000000000000000111470318010063 * Math.pow(Distance, 9) + -0.000000000000000000073545502934 * Math.pow(Distance, 10)) * 1000;
-    // if (Distance <= 75){
-    //   return 0;
-    // }
-
-    // else{
-    //   return -201.0426277257740000000 + 12.39140187109030000000 * Distance + -0.32752075587002500000000000 * Math.pow(Distance, 2) + 0.00481306895081772000000000 * Math.pow(Distance, 3) + -0.00004223018170047660000000 * Math.pow(Distance, 4) + 0.00000021415550566587700000 * Math.pow(Distance, 5) + -0.00000000046497360883538700 * Math.pow(Distance, 6) + -0.000000000001012042357180700000 * Math.pow(Distance, 7) + 0.000000000000009256378555327140 * Math.pow(Distance, 8) + -0.000000000000000022959256981162 * Math.pow(Distance, 9) + 0.000000000000000000020789680208 * Math.pow(Distance, 10);
-    // }
   }
 
   @Override
